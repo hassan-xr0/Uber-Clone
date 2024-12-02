ffdf57622bed586486fdb4b5a1eeb5521f72bd84
Hassan <syedhassan3038@gmail.com>
2024-12-02 21:09:20 +0500

    Authentication for Captain route

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -1,4 +1,5 @@
 const userModel = require('../models/user.model')
+const captainModel = require('../models/captiain.model')
 const blacklistTokenModel = require("../models/blacklistToken.model")
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
@@ -24,3 +25,27 @@ module.exports.authUser= async (req,res,next)=>{
         return res.status(401).json({error:"Unauthorized"})
     }
 }
+
+module.exports.authCaptain= async (req,res,next)=>{
+    const token = req.cookies.token || req.headers.authorization?.split(' ')[1]
+    if(!token){
+        return res.status(401).json({error:"Unauthorized"})
+    }
+    const isBlacklisted = await blacklistTokenModel.findOne({token:token})
+    if(isBlacklisted){
+        return res.status(401).json({messsage:"Unauthorized"})
+    }
+
+    try {
+        const decode = jwt.verify(token,process.env.JWT_SECRET)
+        const captain = await captainModel.findById(decode._id)
+        req.captain = captain;
+
+        return next()
+    }
+        catch(err){
+            console.log(err);
+
+        res.status(401).json({ message: 'Unauthorized' });
+        }
+    }
--- backend/routes/captain.routes.js
+++ backend/routes/captain.routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const captainController = require("../controllers/captain.controller");
 const { body } = require("express-validator");
+authMiddleware = require('../middlewares/auth.middleware')
 const router = express.Router();
 
 router.post('/register',[
@@ -12,5 +13,13 @@ router.post('/register',[
     body('vehicle.capacity').isInt({ min: 1 }).withMessage('Capacity must be at least 1'),
     body('vehicle.vehicleType').isIn([ 'car', 'motorcycle', 'auto' ]).withMessage('Invalid vehicle type')
   ],captainController.registerCaptain);
+
+router.post('/login',[
+  body('email').isEmail().withMessage('Invalid Email'),
+  body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
+],captainController.loginCaptain);
+
+router.get('/profile',authMiddleware.authCaptain ,captainController.getCaptainProfile);
+router.get('/logout',authMiddleware.authCaptain ,captainController.logoutCaptain);
 
 module.exports = router;
