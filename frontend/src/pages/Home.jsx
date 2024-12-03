27e1113a7da3d0c6ee92730e47a571c6aa1f7bf0
Hassan <syedhassan3038@gmail.com>
2024-12-03 16:29:41 +0500

    -

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import uberLogo from '/public/images/uber-logo.png'
 
 
 const Home = () => {
   return (
     <div className="uber-move-text">
-      <div className="h-screen w-full pt-8  bg-[url(https://plus.unsplash.com/premium_photo-1683568217010-e390b4f34737?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)]  bg-cover bg-ceter bg-no-repeat  flex justify-between flex-col">
-      <img className='w-16 ml-8' src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="" />
+      <div className="h-screen w-full pt-8  bg-[url(/public/images/hero-bg.avif)]  bg-cover bg-ceter bg-no-repeat  flex justify-between flex-col">
+      <img className='w-16 ml-8' src={uberLogo} alt="" />
 
         <div className="bg-white py-5  px-7">
           <h2 className="text-3xl tracking-tighter font-semibold">Get Started with Uber</h2>
