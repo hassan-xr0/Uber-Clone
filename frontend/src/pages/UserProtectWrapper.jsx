f6824968f528df8cf0659e6dd41ab24e011bbddb
Hassan <syedhassan3038@gmail.com>
2024-12-24 13:36:42 +0500

    Improve UserProtection components

--- frontend/src/pages/UserProtectWrapper.jsx
+++ frontend/src/pages/UserProtectWrapper.jsx
@@ -1,21 +1,51 @@
-import React from "react";
-import { useContext,useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useContext, useEffect, useState } from 'react'
+import { UserDataContext } from '../context/UserContext'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import { Oval } from 'react-loader-spinner'
 
+const UserProtectWrapper = ({
+    children
+}) => {
+    const token = localStorage.getItem('token')
+    const navigate = useNavigate()
+    const { user, setUser } = useContext(UserDataContext)
+    const [ isLoading, setIsLoading ] = useState(true)
 
-const UserProtectWrapper = ({ children }) => {
-  const token = localStorage.getItem("token");
-  const navigate = useNavigate();
+    useEffect(() => {
+        if (!token) {
+            navigate('/login')
+        }
 
+        axios.get(`${import.meta.env.VITE_BASE_URL}/users/profile`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }).then(response => {
+            if (response.status === 200) {
+                setUser(response.data)
+                setIsLoading(false)
+            }
+        })
+            .catch(err => {
+                console.log(err)
+                localStorage.removeItem('token')
+                navigate('/login')
+            })
+    }, [ token ])
 
-  useEffect(() => {
-    if (!token) {
-      navigate("/login");
+    if (isLoading) {
+        return (
+          <div className='text-center text-xl font-medium' > Loading...</div>
+        )
     }
-  }, [token]);
 
+    return (
+        <>
+            {children}
+            
+        </>
+    )
+}
 
-  return <div>{children}</div>;
-};
-
-export default UserProtectWrapper;
+export default UserProtectWrapper
